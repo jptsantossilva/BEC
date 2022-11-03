f56eb7ae2071ef121c6e5c972a832f9bcfacea26
João Silva <108815251+jptsantossilva@users.noreply.github.com>
2022-11-03 15:50:29 +0000

    crossover
    crossover calculation updated.

--- Prod/Main.py
+++ Prod/Main.py
@@ -15,7 +15,8 @@ import sys
 import math
 import numpy as np
 import dataframe_image as dfi
-
+from numbers import Number
+from typing import Sequence
 
 # %%
 # environment variables
@@ -45,7 +46,7 @@ client = Client(api_key, api_secret)
 gFastMA = int("8")
 gSlowMA = int("34")
 gTimeFrameNum = int("1")
-gtimeframeTypeShort = "h" # hour, day
+gtimeframeTypeShort = "h" # h, D
 gtimeframeTypeLong = "hour" # hour, day
 gStrategyName = str(gFastMA)+"/"+str(gSlowMA)+" CROSS"
 
@@ -250,6 +251,26 @@ def calcPnL(symbol, sellprice: float, sellqty: float):
                 
                 break
 
+def crossover(series1: Sequence, series2: Sequence) -> bool:
+    """
+    Return `True` if `series1` just crossed over (above)
+    `series2`.
+
+        >>> crossover(self.data.Close, self.sma)
+        True
+    """
+    series1 = (
+        series1.values if isinstance(series1, pd.Series) else
+        (series1, series1) if isinstance(series1, Number) else
+        series1)
+    series2 = (
+        series2.values if isinstance(series2, pd.Series) else
+        (series2, series2) if isinstance(series2, Number) else
+        series2)
+    try:
+        return series1[-2] < series2[-2] and series1[-1] > series2[-1]
+    except IndexError:
+        return False
 
 # %%
 def trader():
@@ -263,7 +284,7 @@ def trader():
         # sendTelegramMessage("",coinPair) 
         df = getdata(coinPair)
         applytechnicals(df)
-        lastrow = df.iloc[-1]
+        # lastrow = df.iloc[-1]
 
         # separate coin from stable. example coinPair=BTCUSDT coinOnly=BTC coinStable=USDT 
         coinOnly = coinPair[:-4]
@@ -271,7 +292,8 @@ def trader():
         coinStable = coinPair[-4:]
         # print('coinStable=',coinStable)
 
-        if lastrow.SlowMA > lastrow.FastMA:
+        # if lastrow.SlowMA > lastrow.FastMA:
+        if crossover(df.SlowMA, df.FastMA):
             # sendTelegramMessage("",client.SIDE_SELL+" "+coinPair)
             # print('coinStable=',coinStable) 
             # was not selling because the buy order amount is <> from the balance => fees were applied and we get less than the buy order
@@ -358,8 +380,10 @@ def trader():
         # sendTelegramMessage("",coinPair) 
         df = getdata(coinPair)
         applytechnicals(df)
-        lastrow = df.iloc[-1]
-        if lastrow.FastMA > lastrow.SlowMA:
+        # lastrow = df.iloc[-1]
+        
+        # if lastrow.FastMA > lastrow.SlowMA:
+        if crossover(df.FastMA, df.SlowMA):
             positionSize = calcPositionSize(pStablecoin=coinStable)
             # sendTelegramMessage("", "calc position size 5")
             # print("positionSize: ", positionSize)
