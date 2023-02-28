b689a70e65348128f865f69d0b1ad5f47a0a6db3
João Silva <108815251+jptsantossilva@users.noreply.github.com>
2023-02-28 17:11:10 +0000

    trade against BTC

--- Main.py
+++ Main.py
@@ -231,7 +231,13 @@ def calc_stake_amount(coin):
             telegram.send_telegram_message(telegramToken, telegram.eWarning, msg)
     
         tradable_balance = balance*tradable_balance_ratio 
+        
         stake_amount = tradable_balance/(max_number_of_open_positions-num_open_positions)
+        
+        if coin == "BTC":
+            stake_amount = round(stake_amount, 8)
+        elif coin in ("BUSD", "USDT"):
+            stake_amount = int(stake_amount)
         
         # make sure the size is >= the minimum size
         if stake_amount < min_position_size:
