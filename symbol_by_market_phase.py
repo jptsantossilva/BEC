0835e75e85e799ece4c0c43e75aaaaed55fb1d7a
João Silva <108815251+jptsantossilva@users.noreply.github.com>
2023-05-05 12:41:11 +0100

    create_balance_snapshot

--- symbol_by_market_phase.py
+++ symbol_by_market_phase.py
@@ -1,6 +1,6 @@
 import config
 import pandas as pd
-from datetime import datetime, timedelta
+from datetime import datetime
 import numpy as np
 import sys
 import timeit
@@ -8,7 +8,8 @@ import add_symbol
 import telegram
 import logging
 import database
-from exchange import client, get_exchange_info, get_price_close_by_symbol_and_date
+# from exchange import client, get_exchange_info, create_balance_snapshot
+import exchange
 
 # calculate program run time
 start = timeit.default_timer() 
@@ -53,7 +54,7 @@ def get_blacklist():
 # get blacklist
 blacklist = get_blacklist()
 
-exchange_info = get_exchange_info()
+exchange_info = exchange.get_exchange_info()
         
 symbols = set()
 
@@ -85,7 +86,7 @@ def apply_technicals(df):
 
 def get_data(symbol):
     try:
-        frame = pd.DataFrame(client.get_historical_klines(symbol
+        frame = pd.DataFrame(exchange.client.get_historical_klines(symbol
                                                         ,timeframe                                        
                                                         
                                                         # better get all historical data. 
@@ -113,84 +114,6 @@ def get_data(symbol):
         # return empty dataframe
         frame = pd.DataFrame()
         return frame 
-
-def create_balance_snapshot():
-    msg = "Creating balance snapshot..."
-    print(msg)
-    telegram.send_telegram_message(telegram.telegram_token_market_phases, "", msg)
-
-    last_date = database.get_last_date_from_balances(database.conn)
-    if last_date == '0':
-         today = datetime.now()
-         start_date = today - timedelta(days=30)
-    else:
-         start_date = datetime.strptime(last_date, '%Y-%m-%d')        
-
-    snapshots = client.get_account_snapshot(type="SPOT",
-                                            startTime=int(start_date.timestamp()*1000), 
-                                            # endTime=int(end_date.timestamp()*1000)
-                                            limit=30 #max                                        
-                                            )
-
-    code = snapshots['code']
-    msg = snapshots['msg']
-
-    # get list of available symbols. 
-    # This is usefull to avoid getting price from symbol that do not trade against stable
-    exchange_info = get_exchange_info()
-    symbols = set()
-    trade_against = "BUSD"
-    for s in exchange_info['symbols']:
-        if (s['symbol'].endswith(trade_against)
-            and s['status'] == 'TRADING'):
-                symbols.add(s['symbol']) 
-
-    # Create a Pandas DataFrame to store the daily balances for each asset
-    df_balance = pd.DataFrame()
-
-    # Iterate through the snapshots and get the daily balance for each asset
-    for snapshot in snapshots['snapshotVos']:
-        if snapshot['type'] == 'spot' and snapshot['data'] is not None:
-            snapshot_date = datetime.fromtimestamp(snapshot['updateTime']/1000).date()
-            totalAssetOfBtc = snapshot['data']['totalAssetOfBtc']
-            for balance in snapshot['data']['balances']:
-                asset = balance['asset']
-                daily_balance = float(balance['free'])
-
-                print(f"{snapshot_date}-{asset}")
-                
-                # ignore if balance = 0
-                if daily_balance == 0.0:
-                    continue
-
-                symbol_with_trade_against = asset+trade_against
-
-                if asset in [trade_against]:
-                     balance_usd = daily_balance
-                elif symbol_with_trade_against not in symbols:
-                     print(f"{asset} not in available symbols")
-                     balance_usd = 0
-                else:
-                    # convert snapshot_date from date to datetime
-                    date = datetime.combine(snapshot_date, datetime.min.time())
-                    unit_price = get_price_close_by_symbol_and_date(symbol_with_trade_against, date)
-                    balance_usd = unit_price * daily_balance
-
-                df_new = pd.DataFrame({
-                    'Date': [snapshot_date],
-                    'Asset': [asset],
-                    'Balance': [daily_balance],
-                    'Balance_USD': [balance_usd],
-                    'Total_Balance_Of_BTC': [totalAssetOfBtc]
-                    })
-                # add to total
-                df_balance = pd.concat([df_balance, df_new], ignore_index=True)
-
-    # Print the daily balances for each asset
-    # print(df_balance)
-
-    # add data to table Balance
-    database.add_balances(database.conn, df_balance)
 
 # empty dataframe
 dfResult = pd.DataFrame()
@@ -279,7 +202,7 @@ telegram.send_telegram_file(telegram.telegram_token_market_phases, filename)
 #---------------------------------------------
 
 # save each symbol balance to database to be used on dashboard charts
-create_balance_snapshot()
+exchange.create_balance_snapshot()
 
 if not df_top.empty:
     # remove coins from position files that are not top performers in accumulation or bullish phase
