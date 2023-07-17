e21beae6a0752de0541adf3daf5a3c18a55e9849
João Silva <108815251+jptsantossilva@users.noreply.github.com>
2023-07-17 12:35:53 +0100

    Update add_symbol.py

--- add_symbol.py
+++ add_symbol.py
@@ -19,7 +19,7 @@ def run():
     list_not_completed.index += 1
 
     if not list_not_completed.empty: # not empty 
-        msg = f"{telegram.telegram_prefix_market_phases_sl}Calculating best EMA for the following coins:"
+        msg = f"{telegram.telegram_prefix_market_phases_sl}Calculating best EMA for the following symbols:"
         telegram.send_telegram_message(telegram.telegram_token_main, "", msg)
         msg = telegram.telegram_prefix_market_phases_sl +"\n"+ list_not_completed.to_string(index=True, header = False)
         telegram.send_telegram_message(telegram.telegram_token_main, "", msg) 
