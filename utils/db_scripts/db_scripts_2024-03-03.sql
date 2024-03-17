a57bb37b766901732d603828ce9f880ee46f3705
João Silva <108815251+jptsantossilva@users.noreply.github.com>
2024-03-17 09:19:33 +0000

    update pragma version

--- utils/db_scripts/db_scripts_2024-03-03.sql
+++ utils/db_scripts/db_scripts_2024-03-03.sql
@@ -2,4 +2,4 @@ ALTER TABLE Positions ADD Take_Profit_3 INTEGER NOT NULL DEFAULT 0;
 ALTER TABLE Positions ADD Take_Profit_4 INTEGER NOT NULL DEFAULT 0;
 
 -- update db version
-PRAGMA user_version = 20240317
+PRAGMA user_version = 20240303
